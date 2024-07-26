ed9d6ef6d1cfca22de0e565999e538368eaf553c
danilo333227 <danilochupa2013@gmail.com>
2024-07-26 17:09:58 +0300

    Додано керування вгору та вниз

--- maze.py
+++ maze.py
@@ -11,11 +11,40 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+
+        if keys[K_d] and self.rect.x < 700 - 60:
+            self.rect.x += self.speed
+
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+
+        if keys[K_s] and self.rect.y < 500 - 60:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    direction = False
+
+    def update(self):
+        if not self.direction:
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+        if self.rect.x <= 470:
+            self.direction = True
+        if self.rect.x > 700 - 80:
+            self.direction = False
+
 win_width = 700
 win_height = 500
 
-player = GameSprite("hero.png",5,win_height-80,4)
-enemy = GameSprite("cyborg.png",win_width-80,280,2)
+player = Player("hero.png",5,win_height-80,4)
+enemy = Enemy("cyborg.png",win_width-80,280,2)
 final = GameSprite("treasure.png",win_width-120,win_height-80,0)
 
 window = display.set_mode((win_width,win_height))
@@ -36,9 +65,14 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        window.blit(background,(0,0))
-        player.reset()
-        enemy.reset()
-        final.reset()
-        display.update()
-        clock.tick(FPS)
+
+    window.blit(background,(0,0))
+    player.reset()
+    enemy.reset()
+    final.reset()
+
+    player.update()
+
+
+    display.update()
+    clock.tick(FPS)
